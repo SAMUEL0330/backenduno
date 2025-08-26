9f46e98c5744362c9291319b193a7dbba1e55d18
SAMUEL0330 <samuel.herrera.bedoya@gmail.com>
2025-08-25 19:57:02 -0500

    Inicio del proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Manager manager = new Manager();//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        Manager.addNumero(5);
+        Manager.addNumero(4);
+        Manager.addNumero(3);
+        Manager.addNumero(2);
+        Manager.addNumero(1);
+    }
+}
